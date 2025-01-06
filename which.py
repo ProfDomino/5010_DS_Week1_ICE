5c65f0c825b1d523b524acd5b74e892e98e19af4
ProfDomino <m.domino@northeastern.edu>
2025-01-05 19:46:15 -0500

    Add files via upload

---
+++ which.py
@@ -0,0 +1,41 @@
+# Name:
+# DS5010
+
+# In-class exercise
+# For these exercises: Do NOT use an in-built function 
+# or something that would otherwise trivialize this process.
+
+def mean(values):
+    """
+    Create a function that will take in a list of integers or floats and return the average
+    
+
+    param: values - a list-like structure of numbers (ints or floats)
+    returns: average of all numbers in values
+
+    Ex. [1,2,3] --> 2
+    """
+    pass
+
+def mode(items):
+    """
+    2. Create a function that will take a list of values and return the item that occurs most 
+    frequently within that list
+
+    param: items - a list-like structure of any data type
+    returns: the value wihin items that occurs most frequently
+    Ex. ['c','d',2, False, 2,'d', 'd', 'd', 2 ] --> 'd'
+    """
+    pass
+
+def which(x, value):
+    """
+    Searhcers for all occurances of a value in in a list-like structure
+    param: x - a list-like structure to search for occurances of value
+    param: value - a value to find in x
+    returns: a tuple containing all indicies of value within the list.  
+    Tuple should be empty if x does not contain value
+
+    Ex: which([1, 2, 2, 1, 2], 2) --> (1, 2, 4)
+    """
+    pass
